5763d9c2849b15c9b8225f116e474fd2e465b432
Melvin Finn <melvinrf@Outlook.com>
2021-04-05 15:09:12 -0500

    finished with timer, now on to making different questions who up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # codequiz_mf
+
+In this assignment, we are tasked with coding a quiz that displays multiple pages with questions and has a timer. It also has other requirements, but I can't remember them at the moment.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,27 @@
-var timeEl = document.querySelector(".time");
-var gameState = 0;
+var timeEl = document.getElementById("time")
 
 var secondsLeft = 10;
 
 function setTime() {
+  if (secondsLeft === 0) secondsLeft = 10;
+  timeEl.setAttribute("style", "opacity: 1;")
     var timerInterval = setInterval(function() {
       secondsLeft--;
-      timeEl.textContent = secondsLeft;
-  
-      if(secondsLeft === 0) {
+      timeEl.textContent ="Time: " + secondsLeft;
+
+      if (secondsLeft === 0) {
+        alert("Game Over");
         clearInterval(timerInterval);
-        sendMessage();
+        timeEl.setAttribute("style", "opacity: 0;");
+        return;l
       }
-  
-    }, 1000);
+    }, 1000)
+    return;
   }
 
-  function sendMessage() {
-    timeEl.textContent = secondsLeft;
+  function shuffler() {
+
   }
-  
+
 
   
