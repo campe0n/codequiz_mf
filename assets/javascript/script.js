b572e75687bac4862bd57e0fd5d11af079ca2234
Melvin Finn <melvinrf@Outlook.com>
2021-04-05 12:43:27 -0500

    trying to get my timer function to work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,24 @@
-// BUTTON
+var timeEl = document.querySelector(".time");
+var gameState = 0;
+
+var secondsLeft = 10;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+  function sendMessage() {
+    timeEl.textContent = secondsLeft;
+  }
+  
+
+  
