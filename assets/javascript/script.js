0ded02dad00c0814576551f82ced461f03d7063e
Melvin Finn <melvinrf@Outlook.com>
2021-04-07 15:12:25 -0500

    finihsed all my questions and score is displayed at the end have to work on keeping scores and getting scores

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,8 +5,13 @@ var startDeck = document.querySelector("section");
 var startHeader = document.querySelector("h1");
 var startText = document.getElementById("cardtxt");
 var startBtn = document.getElementById("startButton");
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
+var b4 = document.getElementById("b4");
 
 var secondsLeft = 120;
+var score = 0;
 
 function setTime() {
   if (secondsLeft === 0) secondsLeft = 120;
@@ -28,49 +33,66 @@ function setTime() {
   function setDeck() {
     startText.remove();
     startBtn.remove();
-    startDeck.setAttribute("style", "display: flex; justify-content: flex-start;")
-    let h2 = document.createElement("h1");
+
+    b1.setAttribute("style", "opacity: 1;")
+    b2.setAttribute("style", "opacity: 1;")
+    b3.setAttribute("style", "opacity: 1;")
+    b4.setAttribute("style", "opacity: 1;")
+
+    let h2 = document.createElement("H1");
     let q1 = document.createTextNode('what does the "api" in web api mean?');
     let question1 = h2.appendChild(q1);
     startHeader.replaceWith(question1);
 
-    let q1btn1 = document.createElement("button");
-    q1btn1.innerHTML = "automating program intializer";
-    document.body.children[1].children[0].appendChild(q1btn1);
-    let q1btn2 = document.createElement("button");
-    q1btn2.innerHTML = "automatic program interface";
-    document.body.children[1].children[0].appendChild(q1btn2);
-    let q1btn3 = document.createElement("button");
-    q1btn3.innerHTML = "application programming interface";
-    document.body.children[1].children[0].appendChild(q1btn3);
-    let q1btn4 = document.createElement("button");
-    q1btn4.innerHTML = "application program iterator";
-    document.body.children[1].children[0].appendChild(q1btn4);
+    let a1 = document.createTextNode("automating programming interface");
+    b1.appendChild(a1);
+    let a2 = document.createTextNode("automatic program interface")
+    b2.appendChild(a2);
+    let a3 = document.createTextNode("application programming interface")
+    b3.appendChild(a3);
+    let a4 = document.createTextNode("application program interface")
+    b4.appendChild(a4);
 
-    document.body.children[1].children[0].children[2].addEventListener("click", function() {
-      let h3 = document.createElement("h1");
-      let q2 = document.createTextNode('Which answer is not a primitive data type?');
-      let question2 = h3.appendChild(q2);
-      startHeader.replaceWith(question2);
+    b3.addEventListener('click', function() {
+      score = score + 10;
+      question1.nodeValue = "what does mkdir do in the gitbash terminal?"
+      a1.nodeValue="makes a file"
+      a2.nodeValue="removes a file"
+      a3.nodeValue="makes a directory"
+      a4.nodeValue="makes a dir element"
 
-      let q2btn1 = document.createElement("button");
-      q2btn1.innerHTML = "boolean";
-      document.body.children[1].children[0].appendChild(q2btn1);
-      let q2btn2 = document.createElement("button");
-      q2btn2.innerHTML = "array";
-      document.body.children[1].children[0].appendChild(q2btn2);
-      let q2btn3 = document.createElement("button");
-      q2btn3.innerHTML = "int";
-      document.body.children[1].children[0].appendChild(q2btn3);
-      let q2btn4 = document.createElement("button");
-      q2btn4.innerHTML = "double";
-      document.body.children[1].children[0].appendChild(q2btn4);
+      b3.addEventListener('click', function() {
+        score = score + 10;
+        question1.nodeValue = "what does DOM stand for?"
+        a1.nodeValue="Document Object Model"
+        a2.nodeValue="Do only mode"
+        a3.nodeValue="DOM doesn't stand for anything"
+        a4.nodeValue="document object mode"
 
-      q1btn1.replaceWith(q2btn1);
-      q1btn2.replaceWith(q2btn2);
-      q1btn3.replaceWith(q2btn3);
-      q1btn4.replaceWith(q2btn4);
-    });
-  }
+        b1.addEventListener('click', function() {
+          score = score + 10;
+          question1.nodeValue = "data stored within a local storage is always a _____?"
+          a1.nodeValue="number"
+          a2.nodeValue="float"
+          a3.nodeValue="parseFloat();"
+          a4.nodeValue="string"
+
+          b4.addEventListener('click', function() {
+            score = score + 10;
+            b1.remove();
+            b2.remove();
+            b3.remove();
+            b4.remove();
+            question1.nodeValue="Your Score"
+            var scoreText = document.createElement('H1');
+            var scoreTextNode = document.createTextNode(score);
+            scoreText.appendChild(scoreTextNode);
+            body.appendChild(scoreText)
+          })
+        })
+      })
+    })
+    };
+  
 
   
