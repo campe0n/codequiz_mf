c2d00eff71d806596973a3a555fd34da09702f27
Melvin Finn <melvinrf@Outlook.com>
2021-04-10 14:13:30 -0500

    done with this assignemnt for the week

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,12 +13,13 @@ var b4 = document.getElementById("b4");
 var userInput = document.getElementById("playerinits");
 var submitBtn = document.getElementById('submitBtn');
 var storedInput = localStorage.getItem('saveInput');
-var ul = document.querySelector('ul');
+var ol = document.querySelector('ol');
 var li = document.querySelector('li');
 
 var secondsLeft = 0;
 var score = 0;
 var timerInterval = undefined;
+
 function setTime() {
   if (secondsLeft === 0) secondsLeft = 120;
   timeEl.setAttribute("style", "opacity: 1;")
@@ -35,78 +36,78 @@ function setTime() {
     return;
   }
 
-  function wrongAnswer() {
-    score = score - 10;
-    secondsLeft = secondsLeft - 10;
-    if (secondsLeft < 10) {
-      alert('Game Over')
-      clearInterval(timerInterval);
-      timeEl.setAttribute("style", "opacity: 0;");
-      return;
-    }
+function wrongAnswer() {
+  score = score - 10;
+  secondsLeft = secondsLeft - 10;
+  if (secondsLeft < 10) {
+    alert('Game Over')
+    clearInterval(timerInterval);
+    timeEl.setAttribute("style", "opacity: 0;");
+    return;
   }
+}
 
-  function setDeck() {
-    startText.remove();
-    startBtn.remove();
+function setDeck() {
+  startText.remove();
+  startBtn.remove();
 
-    b1.setAttribute("style", "opacity: 1;")
-    b2.setAttribute("style", "opacity: 1;")
-    b3.setAttribute("style", "opacity: 1;")
-    b4.setAttribute("style", "opacity: 1;")
+  b1.setAttribute("style", "opacity: 1;")
+  b2.setAttribute("style", "opacity: 1;")
+  b3.setAttribute("style", "opacity: 1;")
+  b4.setAttribute("style", "opacity: 1;")
 
-    let question = document.createElement("H1");
-    let qText = document.createTextNode('What does the "api" in web api mean?');
-    question.appendChild(qText);
-    document.body.appendChild(question);
-    startHeader.replaceWith(question);
+  let question = document.createElement("H1");
+  let qText = document.createTextNode('What does the "api" in web api mean?');
+  question.appendChild(qText);
+  document.body.appendChild(question);
+  startHeader.replaceWith(question);
 
-    let a1 = document.createTextNode("automating programming interface");
-    b1.appendChild(a1);
-    let a2 = document.createTextNode("automatic program interface")
-    b2.appendChild(a2);
-    let a3 = document.createTextNode("application programming interface")
-    b3.appendChild(a3);
-    let a4 = document.createTextNode("application program interface")
-    b4.appendChild(a4);
+  let a1 = document.createTextNode("automating programming interface");
+  b1.appendChild(a1);
+  let a2 = document.createTextNode("automatic program interface")
+  b2.appendChild(a2);
+  let a3 = document.createTextNode("application programming interface")
+  b3.appendChild(a3);
+  let a4 = document.createTextNode("application program interface")
+  b4.appendChild(a4);
+
+  b1.addEventListener('click', function() {wrongAnswer();});
+  b2.addEventListener('click', function() {wrongAnswer();});
+  b4.addEventListener('click', function() {wrongAnswer();});
+  b3.addEventListener('click', function() {
+    score = score + 20;
+    qText.nodeValue = "What does mkdir do in the gitbash terminal?"
+    a1.nodeValue="makes a file"
+    a2.nodeValue="removes a file"
+    a3.nodeValue="makes a directory"
+    a4.nodeValue="makes a dir element"
 
     b1.addEventListener('click', function() {wrongAnswer();});
     b2.addEventListener('click', function() {wrongAnswer();});
     b4.addEventListener('click', function() {wrongAnswer();});
     b3.addEventListener('click', function() {
       score = score + 20;
-      qText.nodeValue = "What does mkdir do in the gitbash terminal?"
-      a1.nodeValue="makes a file"
-      a2.nodeValue="removes a file"
-      a3.nodeValue="makes a directory"
-      a4.nodeValue="makes a dir element"
+      qText.nodeValue = "What does DOM stand for?"
+      a1.nodeValue="Document Object Model"
+      a2.nodeValue="Do only mode"
+      a3.nodeValue="DOM doesn't stand for anything"
+      a4.nodeValue="document object mode"
 
-      b1.addEventListener('click', function() {wrongAnswer();});
+      b3.addEventListener('click', function() {wrongAnswer();});
       b2.addEventListener('click', function() {wrongAnswer();});
       b4.addEventListener('click', function() {wrongAnswer();});
-      b3.addEventListener('click', function() {
+      b1.addEventListener('click', function() {
         score = score + 20;
-        qText.nodeValue = "What does DOM stand for?"
-        a1.nodeValue="Document Object Model"
-        a2.nodeValue="Do only mode"
-        a3.nodeValue="DOM doesn't stand for anything"
-        a4.nodeValue="document object mode"
+        qText.nodeValue = "Data stored within a local storage is always a _____?"
+        a1.nodeValue="number"
+        a2.nodeValue="float"
+        a3.nodeValue="parseFloat();"
+        a4.nodeValue="string"
 
-        b3.addEventListener('click', function() {wrongAnswer();});
+        b1.addEventListener('click', function() {wrongAnswer();});
         b2.addEventListener('click', function() {wrongAnswer();});
-        b4.addEventListener('click', function() {wrongAnswer();});
-        b1.addEventListener('click', function() {
-          score = score + 20;
-          qText.nodeValue = "Data stored within a local storage is always a _____?"
-          a1.nodeValue="number"
-          a2.nodeValue="float"
-          a3.nodeValue="parseFloat();"
-          a4.nodeValue="string"
-
-          b1.addEventListener('click', function() {wrongAnswer();});
-          b2.addEventListener('click', function() {wrongAnswer();});
-          b3.addEventListener('click', function() {wrongAnswer();});
-          b4.addEventListener('click', function() {
+        b3.addEventListener('click', function() {wrongAnswer();});
+        b4.addEventListener('click', function() {
             score = score + 20;
             b1.remove();
             b2.remove();
@@ -121,20 +122,21 @@ function setTime() {
             question.setAttribute('style', 'font-size: 40px; font-weight: bold; display: flex; justify-content:center; margin-bottom:2%;');
             submitBtn.setAttribute('style','opacity:1; margin:2%;')
 
-            submitBtn.addEventListener('click', function() {
+            submitBtn.addEventListener('click', function() {  
               var input = document.getElementById('playerinits').value;
               localStorage.setItem("saveInput", input)
               
-              var ul = document.createElement('ul');
-              var li = document.createElement('li');
-              ul.appendChild(li);
-              body.children[1].children[0].appendChild(ul);
-
-
+              localStorage.getItem('saveInput')
+              var li = document.createElement('li')
+              li.textContent = input + "  " + score;
+              ol.appendChild(li); 
+              localStorage.setItem('saveInput', input)
+   
               console.log(input);
             });
           })
         })
       })
     })
-    }; 
+    };
+  
